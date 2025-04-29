30bd58003035b6c38a3051f934ecfd3bfec725c5
iiankyha <zxciiank@gmail.com>
2025-04-29 15:47:36 +0300

    Update anton.py

--- anton.py
+++ anton.py
@@ -1,16 +1,12 @@
-# -*- coding: utf-8 -*-
 from .. import loader, utils
-
 
 @loader.tds
 class AntonMod(loader.Module):
-    """Отвечает &laquo;my def is @seys666228&raquo; со спойлером"""
-
     strings = {"name": "AntonMod"}
 
     async def antoncmd(self, message):
-        # Отправляем сообщение, оборачивая ник в спойлер через HTML-разметку
         await utils.answer(
             message,
             "my def is <tg-spoiler>@seys666228</tg-spoiler>",
-            parse_mode="html",
+            parse_mode="html"
+        )
